f1cff004357ecc0fe4d4395d106063135c5af1f5
Reza Rafiee <gholamreza.rafiee@newcastle.ac.uk>
2017-04-26 14:02:02 +0100

    Add files via upload

--- Master_list_Name_Generator.R
+++ Master_list_Name_Generator.R
@@ -14,17 +14,25 @@ temp = list.files(path= getwd(), pattern="*.gz")
 for (i in 1:length(temp))
  {
   #print(temp[i])
-  #write(temp[i], file = "master_list_1.txt", ncolumns = 2, sep = "\t", append = TRUE)
   pos1 = regexpr('TARGET_EXOME', temp[i])
   pos2 = regexpr('targetExtract', temp[i])
   N1 <- substr(temp[i], pos1[1], pos2[1]-2)
   #print(N1)
-  print(paste("@RG\tID:TP",i,"\tLB:",N1,"\tSM:",N1,"\tPL:ILLUMINA",sep=""))
+  C1 <- i
+  C2 <- paste("@RG\\tID:TP",i,"\\tLB:",N1,"\\tSM:",N1,"\\tPL:ILLUMINA",sep="")
+  C3 <- paste("../FASTQ/",temp[i],sep="")
+  df <- data.frame(C1,C2,C3)
+  write.table(df, file = "master_list.txt", 
+              append = TRUE, sep = "\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
+  
  }
 
 # Output:
 # [1] "@RG\tID:TP1\tLB:TARGET_EXOME_tumor_MBRep_T71_merged\tSM:TARGET_EXOME_tumor_MBRep_T71_merged\tPL:ILLUMINA"
 # [2] ...
+
+# Output:
+# a text file: master_list.txt with three columns for WES analysis
 
 #"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 # End

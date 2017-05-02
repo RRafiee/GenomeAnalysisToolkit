aa0ca433a6fa207637ae2d4ee13368801d7c231f
Reza Rafiee <gholamreza.rafiee@newcastle.ac.uk>
2017-05-02 16:36:45 +0100

    Update Master_list_Name_Generator.R

--- Master_list_Name_Generator.R
+++ Master_list_Name_Generator.R
@@ -43,3 +43,46 @@ for (i in 1:length(temp))
 ##################################################################################################
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##################################################################################################
+setwd("/data/Nuked")
+
+temp = list.files(path= getwd(), pattern="*.gz")
+No_of_sample<- length(temp)/2
+
+for (i in 1:No_of_sample)
+{
+  #print(temp[i])
+  #i <- 1
+  j <- 2*i-1
+  pos11 <- regexpr('New', temp[j]) 
+  pos12 <- regexpr('R', temp[j]) 
+  N1 <- substr(temp[j], pos11[1], pos12[1]-1)
+  
+  pos13 <- regexpr('New', temp[j+1]) 
+  pos14 <- regexpr('R', temp[j+1])
+  N2 <- substr(temp[j+1], pos13[1], pos14[1]-1) #you can get like this: _TXX_""#
+  #print(N1)
+  C1 <- i
+  C2 <- paste("@RG\\tID:WES",i,"\\tLB:Lib_",N1,"\\tSM:MB_cellline",N1,"\\tPL:ILLUMINA",sep="")
+  C3 <- paste("../FASTQ/",temp[j],sep="")
+  C4 <- paste("../FASTQ/",temp[j+1],sep="")
+  df <- data.frame(C1,C2,C3,C4) 
+  write.table(df, file = "master_list.txt", 
+              append = TRUE, sep = "\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
+}
+
+# Output:
+
+# a text file: master_list.txt with four columns for WES analysis
+
+# 1	@RG\tID:IR1\tLB:Lib_01\tSM:ons76_neg\tPL:ILLUMINA	../FASTQ/Newc1_TAAGGCGA_L005_R1_001.fastq.gz    ../FASTQ/Newc1_TAAGGCGA_L005_R2_001.fastq.gz
+# 2	@RG\tID:IR2\tLB:Lib_02\tSM:ons76_54gy_1\tPL:ILLUMINA    ../FASTQ/Newc2_CGTACTAG_L005_R1_001.fastq.gz    ../FASTQ/Newc2_CGTACTAG_L005_R2_001.fastq.gz
+# 3	@RG\tID:IR3\tLB:Lib_03\tSM:ons76_54gy_2\tPL:ILLUMINA    ../FASTQ/Newc3_AGGCAGAA_L005_R1_001.fastq.gz    ../FASTQ/Newc3_AGGCAGAA_L005_R2_001.fastq.gz
+# 4	@RG\tID:IR4\tLB:Lib_04\tSM:ons76_54gy_3\tPL:ILLUMINA    ../FASTQ/Newc4_TCCTGAGC_L005_R1_001.fastq.gz    ../FASTQ/Newc4_TCCTGAGC_L005_R2_001.fastq.gz
+# 5	@RG\tID:IR5\tLB:Lib_05\tSM:uw402_36gy_1\tPL:ILLUMINA    ../FASTQ/Newc5_GGACTCCT_L005_R1_001.fastq.gz    ../FASTQ/Newc5_GGACTCCT_L005_R2_001.fastq.gz
+# 6	@RG\tID:IR6\tLB:Lib_06\tSM:uw402_neg\tPL:ILLUMINA	../FASTQ/Newc6_CTCTCTAC_L005_R1_001.fastq.gz    ../FASTQ/Newc6_CTCTCTAC_L005_R2_001.fastq.gz
+
+#"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+# End
+##################################################################################################
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##################################################################################################
